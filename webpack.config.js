016a9d298bc0a03e9a7700a42c4a4cc84cda5189
Zhang, Harry (LNG-CON) <ZHANGHX@legal.regn.net>
2018-04-17 15:05:53 +0800

    demo draft

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	devtool: 'source-map',
 	entry: __dirname + '/demo6/src/index.js',
 	output:{
-		path:__dirname + '/demo6/public',
-		filename: 'bundle.js'
+		path:__dirname + '/demo6/dist',
+		filename: 'bundle-[hash].js'
 	},
 	devServer: {
-		contentBase: "./demo6/public",
+		contentBase: "/demo6/dist",
 		port:"8090",
-		inline:true,
-		historyApiFallback:true
+		inline:true
 	},
 	module :{
 		rules:[
 			{
 				test: /\.css$/,
-				use: [
-					{
-                       loader: "style-loader",
-					},
-					{
-						loader: "css-loader",
-					}
-				]
+				loader: "style-loader!css-loader"
 			}
 		]
-	}
+	},
+	plugins: [
+		new webpack.BannerPlugin('This is webpack demo'),
+		new HtmlWebpackPlugin({
+			title: 'Harry demo',
+			template: __dirname + "/index.temp.html"
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	]
 }
